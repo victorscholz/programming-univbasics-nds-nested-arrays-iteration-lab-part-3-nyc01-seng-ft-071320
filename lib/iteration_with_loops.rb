f0a5bff0513b900bd098c920fdd701b73a51656d
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-08 03:51:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,15 @@
 def join_nested_strings(src)
-
+  final_string = ""
+  row_index = 0
+  while row_index < src.count do
+      element_index = 0
+      while element_index < src[row_index].count do
+        if src[row_index][element_index].class == String
+          final_string += src[row_index][element_index] + ' '
+        end
+        element_index += 1
+      end
+      row_index += 1
+    end
+    final_string
 end
